48d4c4acd7eb06bbb5c197304da9d69ecfd5efbf
Aidan Jones <Aidan@Aidans-MacBook-Pro.local>
2018-11-04 15:26:08 -0700

    Lightboxes

--- JS/modal.js
+++ JS/modal.js
@@ -1,37 +1,42 @@
 <script>
-  $(document).ready(function() {
-        
-   /* activate the carousel */
-   $("#modal-carousel").carousel({interval:false});
+  // Open the Modal
+  function openModal() {
+    document.getElementById('myModal').style.display = "block";
+  }
 
-   /* change modal title when slide changes */
-   $("#modal-carousel").on("slid.bs.carousel",       function () {
-        $(".modal-title")
-        .html($(this)
-        .find(".active img")
-        .attr("title"));
-   });
+  // Close the Modal
+  function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+  }
 
-   /* when clicking a thumbnail */
-   $(".row .thumbnail").click(function(){
-    var content = $(".carousel-inner");
-    var title = $(".modal-title");
-  
-    content.empty();  
-    title.empty();
-  
-    var id = this.id;  
-     var repo = $("#img-repo .item");
-     var repoCopy = repo.filter("#" + id).clone();
-     var active = repoCopy.first();
-  
-    active.addClass("active");
-    title.html(active.find("img").attr("title"));
-    content.append(repoCopy);
+  var slideIndex = 1;
+  showSlides(slideIndex);
 
-    // show the modal
-    $("#modal-gallery").modal("show");
-  });
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
 
-});
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
 </script>
